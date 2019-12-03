338acfb9266984e3ceb6e031a4ab4fde05f92ed7
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2019-12-03 21:31:31 +0700

    searching movies name from Database method constructed

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -2,11 +2,15 @@ package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
 
+import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.external.MovieData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.wongnai.interview.movie.MovieRepository;
 import com.wongnai.interview.movie.external.MovieDataService;
+
+import java.util.List;
 
 @Component
 public class MovieDataSynchronizer {
@@ -19,5 +23,11 @@ public class MovieDataSynchronizer {
 	@Transactional
 	public void forceSync() {
 		//TODO: implement this to sync movie into repository
+		List<MovieData> movieDatas = movieDataService.fetchAll();
+		for (MovieData data : movieDatas) {
+			Movie movie = new Movie(data.getTitle());
+			movie.getActors().addAll(data.getCast());
+			movieRepository.save(movie);
+		}
 	}
 }
